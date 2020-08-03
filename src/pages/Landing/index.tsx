11e83225172a4ce6300c26463d5f1faf300b1abd
Nayara Ferreira <ncy.fers@gmail.com>
2020-08-03 11:39:22 -0300

    add styles in landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from '../../images/logo.svg';
 import landingImg from '../../images/landing.svg';
+import studyIcon from '../../images/icons/study.svg'
+import giveClasses from '../../images/icons/give-classes.svg'
+import purpleHeart from '../../images/icons/purple-heart.svg'
+import './styles.css';
 
 const Landing: React.FC = () => {
   return (
@@ -12,7 +16,20 @@ const Landing: React.FC = () => {
         </div>
         <img src={landingImg} alt={landingImg} className="hero-image" />
       </div>
+      <div className="buttons-container">
+        <a href="" className="study">
+          <img src={studyIcon} alt="estudar" />
+          Estudar
+        </a>
+        <a href="" className="give-classes">
+          <img src={giveClasses} alt="estudar" />
+          Estudar
+        </a>
+      </div>
 
+      <span className="total-connections">
+        Total de 200 conexões já realizadas  <img src={purpleHeart} alt="coração roxo" />
+      </span>
     </div>
   )
 }
