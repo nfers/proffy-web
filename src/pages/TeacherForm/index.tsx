0aab882450e572727a2ef7d9bfe12b31ad32058e
Nayara Ferreira <ncy.fers@gmail.com>
2020-08-04 17:53:36 -0300

    add api proffy

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import PageHeader from "../../components/PageHeader";
+// import api from "../../services/api";
+
+interface ConnectionsInterface {
+  total: string;
+}
 
 function TeacherForm() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("http://localhost:3030/connections");
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+  console.log(data);
   return (
     <div id="page-teacher-form" className="container">
       <PageHeader title="Que incrível que voce quer dar aulas" />
